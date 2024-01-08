59c7590d0bc2db3c22f31d4b1cf8fbdaa5971946
JoaquinSM13 <89101085+JoaquinSM13@users.noreply.github.com>
2024-01-07 23:41:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -68,14 +68,27 @@ echo '--------------------BSPWM CONFIGURADO----------------------'
 echo '-----------------------------------------------------------'
 
 sudo apt install libpcre3-dev libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-dpms0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson -y
-sudo apt install picom
+sudo apt install -y meson picom libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev libpcre3 libpcre3-dev
+sudo apt install picom -y
 
 echo '-----------------------------------------------------------'
 echo '----------------------PICOM INSTALADO----------------------'
 echo '-----------------------------------------------------------'
 
-sudo apt install rofi -y
 cp kaliBSPWM/picom_d/picom.conf ~/.config/picom.conf
+cd ~/Downloads
+git clone https://github.com/ibhagwan/picom.git
+cd picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+sudo ninja -C build install
+
+echo '-----------------------------------------------------------'
+echo '----------------------PICOM COLOCADO-----------------------'
+echo '-----------------------------------------------------------'
+
+sudo apt install rofi -y
 
 echo '-----------------------------------------------------------'
 echo '----------------------ROFI INSTALADO-----------------------'
@@ -110,13 +123,6 @@ cp ~/kaliBSPWM/lockScreen.png ~/Desktop/images/
 
 echo '-----------------------------------------------------------'
 echo '----------------------FONDO COLOCADO-----------------------'
-echo '-----------------------------------------------------------'
-
-#mkdir ~/.config/picom
-#cp ~/kaliBSPWM/picom_d/picom.conf ~/.config/picom/
-
-echo '-----------------------------------------------------------'
-echo '----------------------PICOM COLOCADO-----------------------'
 echo '-----------------------------------------------------------'
 
 sudo apt install polybar -y
