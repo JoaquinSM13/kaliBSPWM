f07586ffb545a0ae5966afc6de2b1500039d66d4
JoaquinSM13 <89101085+JoaquinSM13@users.noreply.github.com>
2024-01-08 22:53:33 -0500

    Update target_to_hack.sh

--- polybar_d/target_to_hack.sh
+++ polybar_d/target_to_hack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-username='kali'
+username=$(whoami)
 ip_address=$(cat /home/$username/.config/bin/target/target.txt | awk '{print $1}')
 machine_name=$(cat /home/$username/.config/bin/target/target.txt | awk '{print $2}')
  
 if [ $ip_address ] && [ $machine_name ]; then
-    echo "%{F#d81b60} %{F#ffffff}$ip_address%{u-} - $machine_name"
+    echo "%{F#bb72aa} %{F#bb72aa}$ip_address%{u-} - $machine_name"
 else
-    echo "${F#d81b60} %{u-}%{F#ffffff} No target"
+    echo "${F#bb72aa} %{u-}%{F#ffffff} No target"
 fi
 
